9f869854a3a5d9e82ea9c6d78408b7ea214d644e
Wojciech Olszewski <wojciecholszewski87@gmail.com>
2016-01-08 20:25:18 +0100

    SlimAuth required and default parameters & spec

--- spec/SlimAuthSpec.php
+++ spec/SlimAuthSpec.php
@@ -9,13 +9,22 @@ use Psr\Http\Message\ResponseInterface;
 
 class SlimAuthSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable($authenticator)
     {
+        $authenticator->beADoubleOf('Slim\Authenticator\AuthenticatorInterface');
+        $this->beConstructedWith([
+            'authenticator' => $authenticator
+        ]);
+
         $this->shouldHaveType('Slim\SlimAuth');
     }
 
-    function it_is_invocable($request, $response)
+    public function it_is_invocable($authenticator, $request, $response)
     {
+        $authenticator->beADoubleOf('Slim\Authenticator\AuthenticatorInterface');
+        $this->beConstructedWith([
+            'authenticator' => $authenticator
+        ]);
         $request->beADoubleOf('Psr\Http\Message\RequestInterface');
         $response->beADoubleOf('Psr\Http\Message\ResponseInterface');
         $next = function (RequestInterface $request, ResponseInterface $response) {
@@ -23,5 +32,57 @@ class SlimAuthSpec extends ObjectBehavior
         };
 
         $this($request, $response, $next)->shouldReturnAnInstanceOf('Psr\Http\Message\ResponseInterface');
+    }
+
+    public function it_require_authenticator_in_options()
+    {
+        $this->shouldThrow('\RuntimeException')->duringInstantiation();;
+    }
+
+    public function it_require_authenticator_interface_in_options()
+    {
+        $this->beConstructedWith([
+            'authenticator' => new \stdClass()
+        ]);
+
+        $this->shouldThrow('\InvalidArgumentException')->duringInstantiation();
+    }
+
+    public function it_require_rule_interfaces_in_options($authenticator)
+    {
+        $authenticator->beADoubleOf('Slim\Authenticator\AuthenticatorInterface');
+        $this->beConstructedWith([
+            'authenticator' => $authenticator,
+            'rules' => [
+                new \stdClass()
+            ]
+        ]);
+
+        $this->shouldThrow('\InvalidArgumentException')->duringInstantiation();
+    }
+
+    public function it_has_default_options($authenticator)
+    {
+        $authenticator->beADoubleOf('Slim\Authenticator\AuthenticatorInterface');
+        $this->beConstructedWith([
+            'authenticator' => $authenticator
+        ]);
+
+        $rules = $this->getRules();
+
+        $rules->shouldBeArray();
+        $rules->shouldContainsInstanceOf('Slim\Rule\PathRule');
+        $rules->shouldContainsInstanceOf('Slim\Rule\MethodRule');
+    }
+
+    public function getMatchers()
+    {
+        return [
+            'containsInstanceOf' => function ($subject, $interface) {
+                return [] !== array_filter($subject, function ($item) use ($interface) {
+                    return is_a($item, $interface);
+                });
+            },
+        ];
     }
 }
--- src/SlimAuth.php
+++ src/SlimAuth.php
@@ -4,9 +4,31 @@ namespace Slim;
 
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Slim\Authenticator\AuthenticatorInterface;
+use Slim\Rule\MethodRule;
+use Slim\Rule\PathRule;
+use Slim\Rule\RuleInterface;
 
 class SlimAuth
 {
+    /**
+     * @var AuthenticatorInterface
+     */
+    private $authenticator;
+
+    /**
+     * @var RuleInterface[]
+     */
+    private $rules = [];
+
+    /**
+     * @param array $options
+     */
+    public function __construct(array $options = [])
+    {
+        $this->setOptions($options);
+    }
+
     /**
      * @param RequestInterface  $request
      * @param ResponseInterface $response
@@ -15,5 +37,81 @@ class SlimAuth
     public function __invoke(RequestInterface $request, ResponseInterface $response, callable $next)
     {
         return $response;
+    }
+
+    /**
+     * @return AuthenticatorInterface
+     */
+    public function getAuthenticator()
+    {
+        return $this->authenticator;
+    }
+
+    /**
+     * @return RuleInterface[]
+     */
+    public function getRules()
+    {
+        return $this->rules;
+    }
+
+    /**
+     * @param array $options
+     */
+    private function setOptions(array $options)
+    {
+        $this->setAuthenticator($options);
+        $this->setRules($options);
+    }
+
+    /**
+     * @param array $options
+     */
+    private function setAuthenticator(array $options)
+    {
+        if (!array_key_exists('authenticator', $options)) {
+            throw new \RuntimeException('Option "authenticator" is required');
+        }
+
+        if (!is_a($options['authenticator'], 'Slim\Authenticator\AuthenticatorInterface')) {
+            throw new \InvalidArgumentException(sprintf(
+                'Option "authenticator" should be instance of Slim\Authenticator\AuthenticatorInterface, %s given',
+                get_class($options['authenticator'])
+            ));
+        }
+
+        $this->authenticator = $options['authenticator'];
+    }
+
+    /**
+     * @param array $options
+     */
+    private function setRules(array $options)
+    {
+        if (!array_key_exists('rules', $options)) {
+            $this->rules = [
+                new PathRule(),
+                new MethodRule()
+            ];
+        } else {
+            foreach ($options['rules'] as $rule) {
+                $this->addRule($rule);
+            }
+        }
+    }
+
+    /**
+     * @param RuleInterface $rule
+     */
+    private function addRule($rule)
+    {
+        if (!is_a($rule, 'Slim\Rule\RuleInterface')) {
+            throw new \InvalidArgumentException(sprintf(
+                'Option "authenticator" should be instance of Slim\Rule\RuleInterface, %s given',
+                get_class($rule)
+            ));
+        }
+
+        $this->rules[] = $rule;
     }
 }
