07dd99e5c7b1c6dcf773870e30a50e6f992baadd
Wojciech Olszewski <wojciecholszewski87@gmail.com>
2016-01-09 15:58:36 +0100

    AuthenticatorInterface body

--- src/Authenticator/AuthenticatorInterface.php
+++ src/Authenticator/AuthenticatorInterface.php
@@ -2,6 +2,12 @@
 
 namespace Slim\Authenticator;
 
+use Psr\Http\Message\RequestInterface;
+
 interface AuthenticatorInterface
 {
+    /**
+     * @param RequestInterface $request
+     */
+    public function authenticate(RequestInterface $request);
 }
